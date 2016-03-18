066ff694c954aa0a9133f851dcf3758b9503deef
Ashley <ashtraybroom@gmail.com>
2016-03-18 14:58:58 +1100

    index.html nav bar bugfix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("navMenu").innerHTML =
  '<ul>'+
-  '<li><a href="Index.html">HOME</a></li>'+
+  '<li><a href="index.html">HOME</a></li>'+
   '<li><a href="about.html">ABOUT</a></li>'+
   '<li><a href="ideas.html">IDEAS</a></li>'+
   '<li><a href="contact.html">CONTACT</a></li>'+
